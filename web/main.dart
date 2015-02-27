f5a2b2b7b9fe0257f8093d4a2373b31a100f7e5f
ppvk <paul.vankeuren@gmail.com>
2015-02-27 13:47:38 -0600

    added some dart and portfoio bits

---
+++ web/main.dart
@@ -0,0 +1,37 @@
+import 'dart:html';
+import 'dart:async';
+
+List<Element> pages = querySelectorAll('.page');
+Element footer = querySelector('footer');
+
+
+main() {
+  window.onHashChange.listen((_) {
+    setPage();
+  });
+  if (window.location.href.contains('#') == false) window.location.href += '#introduction-page';
+  setPage();
+}
+
+setPage() {
+  String target = window.location.href.split('#')[1];
+  pages.forEach((element) {
+    element.style.opacity = '0';
+    footer.style.opacity = '0';
+  });
+
+  new Timer(new Duration(milliseconds: 500), () {
+    pages.forEach((element) {
+      element.hidden = true;
+
+      Element targetElement = querySelector('#$target');
+      if (targetElement != null) {
+        targetElement.hidden = false;
+        new Timer(new Duration(milliseconds: 500), () {
+          targetElement.style.opacity = "1";
+          footer.style.opacity = '1';
+        });
+      }
+    });
+  });
+}
